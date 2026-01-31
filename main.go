4a1111b4f2aa465f6e9f948eee362cb999e15998
xUfozz <92823013+xUfozz@users.noreply.github.com>
2026-01-31 18:26:16 +0100

    fixed showing dpi and polling rate

--- main.go
+++ main.go
@@ -14,6 +14,15 @@ const (
 	mouseStatusIdx = 6
 )
 
+func presets(dpi int, pollingRate int) (int, int) {
+	// DPI presets: 800, 1200, 1600, 2400
+	// Polling rate presets: 125Hz, 250Hz, 500Hz, 1000Hz
+	dpiPresets := []int{400, 800, 1200, 1600, 2400, 3200, 6400}
+	pollingRatePresets := []int{1000, 500, 250, 125}
+
+	return dpiPresets[dpi], pollingRatePresets[pollingRate]
+
+}
 func main() {
 	dev := hid.Enumerate(vendorID, productID)
 	log.Printf("Found devices: %d", len(dev))
@@ -40,10 +49,14 @@ func main() {
 		if n > 0 && buf[0] == 0x09 {
 			log.Printf("Received data: % X", buf[:n])
 			batteryLevel := int16(buf[1])
-			pollingRate := buf[2]
+			pollingDpiData := buf[2]
+			pollingRatePreset := int(pollingDpiData & 0x0F)
+			dpiPreset := int((pollingDpiData & 0xF0) >> 4)
+			Dpi, pollingRate := presets(dpiPreset, pollingRatePreset)
 
-			log.Printf("Battery level: %d", batteryLevel)
+			log.Printf("Battery: %d", batteryLevel)
 			log.Printf("Polling rate: %d", pollingRate)
+			log.Printf("DPI: %d", Dpi)
 		}
 
 	}
